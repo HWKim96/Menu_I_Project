05e44ccef7132b04e858c978c1903aa0f1d136e6
soo <126047958+sumiiiin@users.noreply.github.com>
2023-04-10 21:31:07 +0900

    Update apps.py

--- common/apps.py
+++ common/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig #AppConfig 클래스를 가져오기
 
 
 class CommonConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField' # 데이터 타입 지정(64-bit 범위의 정수 값)
     name = 'common'
